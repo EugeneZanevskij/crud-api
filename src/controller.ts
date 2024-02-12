13530a993421e13c21393925f8475b0e60372687
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-13 01:57:02 +0300

    feat: update Controller

--- src/controller.ts
+++ src/controller.ts
@@ -1,5 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
 import { user } from './user/user';
+import { getBody } from './helpers/getBody';
+import { isBodyRequired } from './helpers/isBodyRequired';
 
 class Controller {
   getAll(response: ServerResponse) {
@@ -7,8 +9,23 @@ class Controller {
       const users = user.getAll();
       response.writeHead(200, { 'Content-Type': 'application/json' });
       response.end(JSON.stringify(users));
+    } catch (error) {
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: 500, message: 'Server error' }));
     }
-    catch (error) {
+  }
+
+  async create(request: IncomingMessage, response: ServerResponse) {
+    try {
+      const body = await getBody(request, response);
+      const isValid = isBodyRequired(response, body);
+      if (isValid) {
+        const createdUser = user.create(body);
+        response.writeHead(201, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(createdUser));
+        return;
+      }    
+    } catch {
       response.writeHead(500, { 'Content-Type': 'application/json' });
       response.end(JSON.stringify({ code: 500, message: 'Server error' }));
     }
